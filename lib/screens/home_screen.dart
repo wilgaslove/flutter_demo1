33a9899d8baaf7b690217dac8c7b2215e9a0e2d5
wilgaslove <wilfried216@gmail.com>
2023-11-21 14:42:08 +0100

    ajout_floatan_menu_création-d'hashtag

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,8 +8,9 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
- late final PageController _pageController;
+  late final PageController _pageController;
   int _currentIndex = 1;
 
   @override
@@ -26,38 +27,83 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  // Menu pour la création d'un nouveau hashtag.
+  void _modal(BuildContext context) => showModalBottomSheet(
+      context: context,
+      builder: (context) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Wrap(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Créateur d'hashtag",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                  // composant d'entrer hashtag
+                  decoration: InputDecoration(
+                    hintText: "Entrez votre hashtag",
+                  ),
+                ), 
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end, //pour l'alignement
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Cancel".toUpperCase()),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("add".toUpperCase()),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:const Text('Home'),
+          title: const Text('Home'),
         ),
         body: PageView(
-          onPageChanged: (index){
+          onPageChanged: (index) {
             setState(() => _currentIndex = index);
-          } ,
+          },
           controller: _pageController,
-          children: const <Widget> [
+          children: const <Widget>[
             HomeVieuw(),
-            ListHashtagView(), 
+            ListHashtagView(),
           ],
         ),
+// Création du button floatan +
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _modal(context),
+          child: const Icon(
+            Icons.add,
+          ),
+        ),
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap:(index) {
-            setState(() => _currentIndex = index);
-            _pageController.jumpToPage(_currentIndex);
-          }, 
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() => _currentIndex = index);
+              _pageController.jumpToPage(_currentIndex);
+            },
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.list ),
-                label: 'Hashtags'),
-          ]
-        )
-    );
+                  icon: Icon(Icons.list), label: 'Hashtags'),
+            ]));
   }
-} 
+}
